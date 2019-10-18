ee0e3f90272f4cabe7d0aee35c79ad1938a4dd39
GimYoungPhil <youngphil.gim@gmail.com>
2019-10-18 14:23:51 +0900

    add module

---
+++ src/store/modules/yachelinGuide.js
@@ -0,0 +1,51 @@
+import api from '@/api';
+import {
+  REQUEST_LIST__YACHELIN_GUIDE,
+  RECEIVE_LIST__YACHELIN_GUIDE,
+  FAILURE_LIST__YACHELIN_GUIDE,
+} from '@/store/mutation-types';
+
+const state = {
+  list: [],
+  loading: false,
+  error: {},
+};
+
+const getters = {
+};
+
+const mutations = {
+  [REQUEST_LIST__YACHELIN_GUIDE] (state) {
+    state.loading = true;
+  },
+  [RECEIVE_LIST__YACHELIN_GUIDE] (state, data) {
+    state.list = [ ...data ];
+    state.loading = false;
+  },
+  [FAILURE_LIST__YACHELIN_GUIDE] (state, error) {
+    state.error = { ...error };
+    state.loading = false;
+  },
+};
+
+const actions = {
+  requestList({ state, commit }) {
+    if (state.list.length) {
+      return;
+    }
+
+    commit(REQUEST_LIST__YACHELIN_GUIDE);
+    api.get('/ya-assistant.json').then(
+      response => commit(RECEIVE_LIST__YACHELIN_GUIDE, response.date),
+      error => commit(FAILURE_LIST__YACHELIN_GUIDE, error.response.data),
+    );
+  },
+};
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  mutations,
+  actions,
+};
