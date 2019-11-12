e6a78a7d32c260aa86cb65a551fdce67c3ca4a49
bomber <youngphil.gim@gmail.com>
2019-11-12 23:22:47 +0900

    add TochelinGuideRows

---
+++ src/components/tochelinGuide/TochelinGuideRows.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="">
+    <div class="row no-gutters border-top py-3" v-for="(item, index) in list" :key="index">
+      <div class="col-12 col-md-3 h4 mb-md-0">{{ item.toTitle }}</div>
+      <div class="col-12 col-md-9">
+        <div class="row">
+          <div class="col-6 col-sm-4 col-md-2 pt-1">{{ item.membership ? '회원제' : '비회원제' }}</div>
+          <div class="col-6 col-sm-4 col-md-2 pt-1">{{ item.magnet ? '자석O': '자석X' }}</div>
+          <div class="col-6 col-sm-4 col-md-2 pt-1">{{ item.nonCommercial ? '광고X' : '광고O' }}</div>
+          <div class="col-6 col-sm-4 col-md-2 pt-1">{{ item.adultContents ? '성인X' : '성인O' }}</div>
+          <div class="col-6 col-sm-4 col-md-2 pt-1">{{ item.rookie ? '신규' : '기존' }}</div>
+          <div class="col-6 col-sm-4 col-md-2 pt-1">{{ item.status | status }}</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TochelinGuideRows',
+  props: {
+    names: {
+      type: Object,
+      required: true,
+    },
+    list: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
--- src/views/TochelinGuide.vue
+++ src/views/TochelinGuide.vue
@@ -1,26 +1,29 @@
 <template>
   <div class="container">
     <TochelinGuideFilters/>
-    <TochelinGuideTable :names="namedFiltes" :list="filteredList"/>
-    <TochelinGuideMedia :list="filteredList"/>
-    <TochelinGuideCard :list="filteredList"/>
+    <!-- <TochelinGuideTable :names="namedFiltes" :list="filteredList"/> -->
+    <TochelinGuideRows :names="namedFiltes" :list="filteredList"/>
+    <!-- <TochelinGuideMedia :list="filteredList"/> -->
+    <!-- <TochelinGuideCard :list="filteredList"/> -->
   </div>
 </template>
 
 <script>
 import { mapState, mapActions, mapGetters } from 'vuex';
 import TochelinGuideFilters from './TochelinGuideFilters.vue';
-import TochelinGuideTable from '@/components/tochelinGuide/TochelinGuideTable.vue';
-import TochelinGuideMedia from '@/components/tochelinGuide/TochelinGuideMedia.vue';
-import TochelinGuideCard from '@/components/tochelinGuide/TochelinGuideCard.vue';
+// import TochelinGuideTable from '@/components/tochelinGuide/TochelinGuideTable.vue';
+import TochelinGuideRows from '@/components/tochelinGuide/TochelinGuideRows.vue';
+// import TochelinGuideMedia from '@/components/tochelinGuide/TochelinGuideMedia.vue';
+// import TochelinGuideCard from '@/components/tochelinGuide/TochelinGuideCard.vue';
 
 export default {
   name: 'TochelinGuide',
   components: {
     TochelinGuideFilters,
-    TochelinGuideTable,
-    TochelinGuideMedia,
-    TochelinGuideCard,
+    // TochelinGuideTable,
+    TochelinGuideRows,
+    // TochelinGuideMedia,
+    // TochelinGuideCard,
   },
   computed: {
     ...mapState('tochelinGuide', [
